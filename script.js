d7201f94e70ad75ae67d22dff987d95bbfc70018
Altin Thaqi <ltin.thaqi.520@gmail.com>
2021-02-28 17:43:50 +0100

    Added Roll Dice functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+//Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+let currentScore = 0;
+
+//Rolling Dice Functionality
+btnRoll.addEventListener('click', () => {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore; //Change Later
+  } else {
+  }
+});
