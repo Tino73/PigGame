a81a262b623b5de262073f9adc1a6872dfa25487
Altin Thaqi <ltin.thaqi.520@gmail.com>
2021-03-01 16:41:55 +0100

    Added functionality of 2 players

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 //Selecting Elements
+const player0EL = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
@@ -14,7 +17,10 @@ const current1El = document.querySelector('#current--1');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling Dice Functionality
 btnRoll.addEventListener('click', () => {
@@ -26,7 +32,18 @@ btnRoll.addEventListener('click', () => {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore; //Change Later
+    document.querySelector(
+      `#current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
+    currentScore = 0;
+
+    document.querySelector(
+      `#current--${activePlayer}`
+    ).textContent = currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle('player--active');
+
+    player1El.classList.toggle('player--active');
   }
 });
